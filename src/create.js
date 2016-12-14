cc5d05f9ec1add539e63762ff3577834c699d127
zhaoyuu <zhaoyuu@yonyou.com>
2016-12-14 14:37:10 +0800

    value for doc

--- src/create.js
+++ src/create.js
@@ -80,7 +80,9 @@ var testComponentjs = [
 ].join('\n');
 
 var docsContent = [
-    "### API",
+    "# "+name,
+    "## 代码演示",
+    "## API",
     "|参数|说明|类型|默认值|",
     "|:---|:----:|:---:|------:|"
 ].join('\n');
