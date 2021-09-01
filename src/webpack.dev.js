918b8475916a212b425f93e24b6e55ee9cd1a8e8
gx <gaox2025f@gmail.com>
2021-09-01 13:54:37 +0800

    fix: 解析tinper/next-ui包

--- src/webpack.dev.js
+++ src/webpack.dev.js
@@ -2,8 +2,15 @@ var fs = require('fs');
 var webpack = require('webpack');
 var path = require('path');
 
+var node_modules_whitelist = [ // 需要webpack解析的库
+    path.join(process.cwd(), './node_modules/lodash-es'),
+    path.join(process.cwd(), './node_modules/@tinper/next-ui')
+]
 
 function getLoaderExclude(path) {
+    if (!!path.match("@tinper/next-ui") || !!path.match(/lodash-es/)) {
+        return false
+    }
     var isNpmModule = !!path.match(/node_modules/);
     return isNpmModule;
 }
@@ -25,7 +32,7 @@ module.exports = {
                 test: /\.js(x)*$/,
                 // npm modules 都不需要经过babel解析
                 exclude: getLoaderExclude,
-                include: [path.join(process.cwd(), './src'), path.join(process.cwd(), './demo'), path.join(process.cwd(), './test')],
+                include: [path.join(process.cwd(), './src'), path.join(process.cwd(), './demo'), path.join(process.cwd(), './test'), ...node_modules_whitelist],
                 loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015-ie', 'stage-1'].map(function(item) {
